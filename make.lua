01a521a2426cb29d7fd03c7c7ba468de91b0a277
Matthew Brooks <mb964033@gmail.com>
2024-01-25 04:52:19 +0000

    update santoku

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
 
   name = "santoku-fs",
-  version = "0.0.13-1",
+  version = "0.0.14-1",
   variable_prefix = "TK_FS",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.158-1",
+    "santoku >= 0.0.161-1",
   },
 
   test = {
