798726b8b289e7795a6c2f1cd18ee0a9912b9e59
broma0 <121064585+broma0@users.noreply.github.com>
2025-11-24 22:42:48 -0500

    bump version to 0.0.35-1

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
 
   name = "santoku-fs",
-  version = "0.0.34-1",
+  version = "0.0.35-1",
   variable_prefix = "TK_FS",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.245-1",
+    "santoku >= 0.0.297-1",
   },
 
   test = {
@@ -24,6 +24,6 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-  type = "lib",
+
   env = env,
 }
