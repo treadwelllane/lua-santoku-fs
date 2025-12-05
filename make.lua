674d295b7a276d8b3cae04c82d2679bc6b8d133a
Matthew Brooks <mb964033@gmail.com>
2025-12-05 18:13:53 -0500

    update

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
 
   name = "santoku-fs",
-  version = "0.0.35-1",
+  version = "0.0.36-1",
   variable_prefix = "TK_FS",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.297-1",
+    "santoku >= 0.0.303-1",
   },
 
   test = {
@@ -24,6 +24,5 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
-
   env = env,
 }
